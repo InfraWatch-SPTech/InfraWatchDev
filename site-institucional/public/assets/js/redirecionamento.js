eb6db4e9e9cd5966cd901366af4d866dbbd7c255
caiopicciarelli2 <caio.psilva@sptech.school>
2026-09-06 22:05:50 -0300

    refactor(main): Padronizando todas as páginas.

--- site-institucional/public/assets/js/redirecionamento.js
+++ site-institucional/public/assets/js/redirecionamento.js
@@ -1,3 +1,7 @@
 function redirecionamento_login(){
     window.location.href = "../public/login.html";
 }
+
+function redirecionamento_cadastroServidor(){
+    window.location.href = "../public/hardwares.html";
+}
