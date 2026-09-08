165fa6d0fec8a4beb526f1be289c631bc0242e94
caiopicciarelli2 <caio.psilva@sptech.school>
2026-09-08 12:17:37 -0300

    feat(feature/frontend-details): Adicionando um botão home no login

--- site-institucional/public/assets/js/redirecionamento.js
+++ site-institucional/public/assets/js/redirecionamento.js
@@ -2,6 +2,10 @@ function redirecionamento_login(){
     window.location.href = "../public/login.html";
 }
 
+function redirecionamento_home(){
+    window.location.href = "../public/main.html";
+}
+
 function redirecionamento_cadastroServidor(){
     window.location.href = "../public/hardwares.html";
 }
